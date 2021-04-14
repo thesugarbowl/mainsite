0ace09c27e79a8ac285d7634026d48e9f92a9697
patricialan <54603331+patricialan@users.noreply.github.com>
2021-04-14 10:41:20 -0600

    added compression

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var compression = require('compression');
+var compression = require('compression');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,7 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.use(compression()); // Compress all routes
+app.use(compression()); // Compress all routes
 
 app.use(logger('dev'));
 app.use(express.json());
